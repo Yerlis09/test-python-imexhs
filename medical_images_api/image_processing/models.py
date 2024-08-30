982103d2c3ba46260fbb5e160ba8a419ff566a52
Yerlis09 <castellaryerlis18@gmail.com>
2024-08-30 16:26:48 -0500

    feat: Agregar modelo MedicalImageResult para manejo de datos de imágenes médicas
    - Se creo el modelo MedicalImageResult para almacenar datos del dispositivo y resultados del procesamiento de imágenes.
    - Los campos incluyen device_name, mean_before_normalization, mean_after_normalization, data_size, raw_data, created_at y updated_at.
    - Aplicada la migración inicial para crear la tabla correspondiente en la base de datos.

--- medical_images_api/image_processing/models.py
+++ medical_images_api/image_processing/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class MedicalImageResult(models.Model):
+    device_name = models.CharField(max_length=255)
+    mean_before_normalization = models.FloatField()
+    mean_after_normalization = models.FloatField()
+    data_size = models.IntegerField()
+    raw_data = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.device_name} - {self.id}"
